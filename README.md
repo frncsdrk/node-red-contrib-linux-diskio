f3d2c0931ff45f547c30706bcdf70272d9358e95
frncsdrk <carlsmoh@gmail.com>
2018-08-18 19:06:17 +0200

    Add collecting of read/write per second
    update readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# node-red-contrib-linux-diskio
+node-red-contrib-linux-diskio
+===
+
 Collect linux disk io stats
+
+## Credits
+
+[systeminformation](https://github.com/sebhildebrandt/systeminformation)
---
+++ diskio.js
@@ -0,0 +1,32 @@
+module.exports = function (RED) {
+  const si = require('systeminformation')
+
+  function DiskIoNode (conf) {
+    RED.nodes.createNode(this, conf)
+
+    this.name = conf.name
+
+    const node = this
+
+    node.on('input', (msg) => {
+      si.disksIO()
+        .then(data => {
+          let payloadArr = []
+          payloadArr.push({
+            payload: data.rIO_sec,
+            topic: 'diskio_read_sec'
+          })
+          payloadArr.push({
+            payload: data.wIO_sec,
+            topic: 'diskio_write_sec'
+          })
+          node.send([ payloadArr ])
+        })
+        .catch(err => {
+          node.error('Error', err.message)
+        })
+    })
+  }
+
+  RED.nodes.registerType('diskio', DiskIoNode)
+}
